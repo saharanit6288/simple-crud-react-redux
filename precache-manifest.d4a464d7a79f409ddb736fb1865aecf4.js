f88a582993876442ec6dc7b91ee557dd443c3b78
Ranit Saha <ranit.saha@cognizant.com>
2020-03-17 15:17:37 +0530

    Updates

---
+++ precache-manifest.d4a464d7a79f409ddb736fb1865aecf4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdea6256cbd6fd71cb03b83a05404357",
+    "url": "/crud-redux/index.html"
+  },
+  {
+    "revision": "eab91df16a2712fb5851",
+    "url": "/crud-redux/static/css/main.fbbc7933.chunk.css"
+  },
+  {
+    "revision": "4ca3729fa9f4c2533690",
+    "url": "/crud-redux/static/js/2.6eaf2c42.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/crud-redux/static/js/2.6eaf2c42.chunk.js.LICENSE"
+  },
+  {
+    "revision": "eab91df16a2712fb5851",
+    "url": "/crud-redux/static/js/main.8e9422a3.chunk.js"
+  },
+  {
+    "revision": "eac07ae5c7422660df2f",
+    "url": "/crud-redux/static/js/runtime-main.6be8e748.js"
+  }
+]);
